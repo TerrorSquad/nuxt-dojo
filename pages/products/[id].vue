9c470137f0688a2b754459048307eaddab23405f
Goran Ninkovic <g.ninkovic@angeltech.rs>
2024-02-27 15:14:20 +0100

    update product pages to print product data using the product types

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,12 +1,24 @@
 <template>
-  <div>Product details for product ID: {{ id }}</div>
+  <div v-if="product">
+    <p>{{ product.title }}</p>
+    <p>{{ product.price }}</p>
+    <p>{{ product.id }}</p>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import type { Product } from "~/types/product.d.ts"
+
 definePageMeta({
   layout: "products",
 })
 
 const route = useRoute()
 const id = route.params.id
+
+// fetch the product
+const uri = `https://fakestoreapi.com/products/${id}`
+const { data: product } = await useFetch<Product>(uri, {
+  key: `${id}`,
+})
 </script>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>Products</div>
+  <div>
+    <div class="grid grid-cols-4 gap-5">
+      <div
+        v-for="product in products"
+        :key="product.id"
+        class="bg-white p-4 shadow-md"
+      >
+        <NuxtLink :to="`/products/${product.id}`">
+          <img
+            :src="product.image"
+            :alt="product.title"
+            class="w-ful h-48 object-cover"
+          />
+          <h2 class="mt-2 text-lg font-bold">{{ product.title }}</h2>
+          <p class="text-gray-500">{{ product.price }}</p>
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import type { Product } from "~/types/product.d.ts"
 definePageMeta({
   layout: "products",
 })
+const { data: products } = await useFetch<Product[]>("https://fakestoreapi.com/products")
 </script>
-
-<style></style>
